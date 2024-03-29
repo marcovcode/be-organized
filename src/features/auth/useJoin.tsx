1093432c13eee5970c44178f3cd8cf704dbc4fb3
Marco Vidali <marcovidali1@skiff.com>
2024-03-29 08:14:14 +0100

    Move join toast code

--- src/features/auth/useJoin.tsx
+++ src/features/auth/useJoin.tsx
@@ -1,10 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { sendMagicLink } from "../../services/apiAuth";
+import toast from "react-hot-toast";
 
 export function useJoin() {
     const { mutate: join, isPending } = useMutation({
         mutationFn: (email: string) => sendMagicLink(email),
         mutationKey: ["user"],
+
+        onSuccess: () =>
+            toast.success("A verification email has been sent to you"),
+        onError: () => toast.error("An error has occurred while joining"),
     });
 
     return { join, isPending };
--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,14 +1,11 @@
 import { supabase } from "./supabase";
-import toast from "react-hot-toast";
 
 export async function sendMagicLink(email: string) {
     const { data, error } = await supabase.auth.signInWithOtp({
         email: email,
     });
 
-    if (error) toast.error("An error has occurred while joining");
-    else if (!error) toast.success("A verification email has been sent to you");
-
+    if (error) throw new Error(error.message);
     return data;
 }
 
