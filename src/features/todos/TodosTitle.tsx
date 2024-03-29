2012220da6674d3ffe9fdccc42f8cd1a5b974b7e
Marco Vidali <marcovidali1@skiff.com>
2024-03-29 10:54:19 +0100

    Refactor TodosTitle

--- src/features/todos/TodosTitle.tsx
+++ src/features/todos/TodosTitle.tsx
@@ -2,9 +2,10 @@ import { useTodos } from "./useTodos";
 
 function TodosTitle() {
     const { todos } = useTodos();
+
     return (
         <h1 className="font-semibold">
-            Your todos{" "}
+            Your todos
             <span className="text-zinc-400"> - {todos!.length} items</span>
         </h1>
     );
