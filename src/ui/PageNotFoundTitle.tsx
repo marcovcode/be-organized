a9c216994d502562e40724278a322c43eee68205
Marco Vidali <marcovidali1@skiff.com>
2024-04-01 11:52:44 +0200

    Responsive 404 page

--- src/ui/PageNotFoundTitle.tsx
+++ src/ui/PageNotFoundTitle.tsx
@@ -1,6 +1,6 @@
 function PageNotFoundTitle() {
     return (
-        <p className="flex items-center gap-2 text-4xl font-semibold">
+        <p className="flex flex-col items-center gap-2 text-center text-4xl font-semibold sm:flex-row">
             404
             <span className="text-base font-normal">
                 The page you're looking for wasn't found...
